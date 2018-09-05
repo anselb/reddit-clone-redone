49b9a0957aa8ae39cd9db6d376820531d1fe2707
Ansel Bridgewater <iamansel@gmail.com>
2018-09-04 22:43:02 -0700

    Added route and view for showing individual posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,12 +1,22 @@
 const Post = require('../models/post')
 
 module.exports = (app) => {
-  //GET
+  //Get individual post
+  app.get('/posts/:id', (req, res) => {
+    //Look up post
+    Post.findById(req.params.id).then((post) => {
+      res.render('posts-show.hbs', { post })
+    }).catch((err) => {
+      console.log(err.message)
+    })
+  })
+
+  //Get posts creation page
   app.get('/posts/new', (req, res) => {
     res.render('posts-new')
   })
 
-  //CREATE
+  //Create a new post
   app.post('/posts/', (req, res) => {
     //Instatiate instance of post model
     var post = new Post(req.body)
