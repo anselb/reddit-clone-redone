69bfbd261f932534b5ccbc8f688bf8882a9ce2a9
Ansel Bridgewater <iamansel@gmail.com>
2018-09-05 21:26:53 -0700

    Added password salting and hashing

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
@@ -9,12 +10,30 @@ const UserSchema = new Schema({
 })
 
 UserSchema.pre('save', function(next) {
+  //Set created at and updated at times
   const now = new Date()
   this.updatedAt = now
   if (!this.createdAt) {
     this.createdAt = now
   }
-  next()
+
+  //Encrypt password
+  const user = this
+  if (!user.isModified('password')) {
+    return next()
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      user.password = hash
+      next()
+    })
+  })
 })
+
+UserSchema.methods.comparePassword = (password, done) => {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    done(err, isMatch)
+  })
+}
 
 module.exports = mongoose.model('User', UserSchema)
