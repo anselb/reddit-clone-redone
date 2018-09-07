386f5c275dc8117ff2b7c0da5e53bc293184b1fe
Ansel Bridgewater <iamansel@gmail.com>
2018-09-07 11:33:08 -0700

    Added login and logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,4 +20,40 @@ module.exports = (app) => {
       return res.status(400).send({ err: err })
     })
   })
+
+  //Get login form
+  app.get('/login', (req, res) => {
+    res.render('login.hbs')
+  })
+
+  //Login
+  app.post('/login', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+    User.findOne({ username }, 'username password').then((user) => {
+      if (!user) {
+        return res.status(401).send({ message: 'Wrong Username or Password' })
+      }
+      user.comparePassword(password, (err, isMatch) => {
+        if (!isMatch) {
+          console.log(password);
+          return res.status(401).send({ message: 'Wrong Username or Password' })
+        }
+        const token = jwt.sign(
+          { _id: user._id, username: user.username }, process.env.SECRET,
+          { expiresIn: "60 days" }
+        )
+        res.cookie('nToken', token, { maxAge: 900000, httpOnly: true })
+        res.redirect('/')
+      })
+    }).catch((err) => {
+      console.log(err)
+    })
+  })
+
+  //Logout
+  app.get('/logout', (req, res) => {
+    res.clearCookie('nToken')
+    res.redirect('/')
+  })
 }
